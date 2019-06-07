6d0e008c9e6904ba39d231b6777c524b38a1a1a9
serdarb <hserdarb@gmail.com>
2019-06-07 11:06:38 +0300

    Create IStandardCache.cs

---
+++ Sources/StandardCache/IStandardCache.cs
@@ -0,0 +1,11 @@
+﻿namespace StandardCache
+{
+    public interface IStandardCache
+    {
+        void AddOrUpdate(string category, CacheItem item);
+        CacheItem Get(string category, string key);
+        void Remove(string category, string key);
+        void ClearExpired(string category);
+        void Clear(string category);
+    }
+}
